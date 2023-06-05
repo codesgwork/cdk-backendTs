d1826f75bc2d3ad3016459d77d3cccaa8a121eb7
skyGwork <skyGworkk@gmail.com>
2023-06-06 00:24:40 +0530

    update

---
+++ README.md
@@ -0,0 +1,15 @@
+# Welcome to your CDK TypeScript project
+
+This is a blank project for CDK development with TypeScript.
+
+The `cdk.json` file tells the CDK Toolkit how to execute your app.
+
+## Useful commands
+`cdk bootstrap` : Deploys the CDK Toolkit staging stack; see Bootstrapping
+`cdk synth`       emits the synthesized CloudFormation template
+`cdk deploy`      deploy this stack to your default AWS account/region
+
+* `npm run build`   compile typescript to js
+* `npm run watch`   watch for changes and compile
+* `npm run test`    perform the jest unit tests
+* `cdk diff`        compare deployed stack with current state
---
+++ lib/cdk-backend_ts-stack.ts
@@ -0,0 +1,51 @@
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+import * as cdk from 'aws-cdk-lib';
+import { Bucket, CfnBucket } from 'aws-cdk-lib/aws-s3';
+import { Construct } from 'constructs';
+
+class L3Bucket extends Construct {
+  constructor(scope: Construct, id: string, expiration: number) {
+    super(scope, id);
+    new Bucket(this, 'L3Bucket', {
+      lifecycleRules: [
+        {
+          expiration: cdk.Duration.days(expiration),
+        },
+      ],
+    });
+  }
+}
+
+export class CdkBackendTsStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // create an s3 bucket 3 ways
+
+    new CfnBucket(this, 'MyL1Bucket', {
+      lifecycleConfiguration: {
+        rules: [
+          {
+            expirationInDays: 1,
+            status: 'Enabled',
+          },
+        ],
+      },
+    });
+
+    const MyL2Bucket = new Bucket(this, 'MyL2Bucket', {
+      lifecycleRules: [
+        {
+          expiration: cdk.Duration.days(2),
+        },
+      ],
+    });
+
+    new L3Bucket(this, 'MyL3Bucket', 3);
+
+    new cdk.CfnOutput(this, 'MyL2BucketName', {
+      value: MyL2Bucket.bucketName,
+    });
+  }
+}
